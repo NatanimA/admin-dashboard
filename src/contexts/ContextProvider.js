fb112fb517217977f2d6200ce5da315b0ffc76dc
NatanimA <Natanimabesha@gmail.com>
2023-06-30 01:20:39 +0300

    ContextProvider Insertions

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState } from "react";
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  return (
+    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
