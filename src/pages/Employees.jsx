a94121b7f99c5a01bd4a146a3ddb07760506a8bd
NatanimA <Natanimabesha@gmail.com>
2023-07-01 01:47:51 +0300

    Employees Component Insertions

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Search,
+  Page,
+  Toolbar,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
 
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="Page" title="Employees" />
+        <GridComponent
+          id="gridcomp"
+          dataSource={employeesData}
+          toolbar={["Search"]}
+          allowPaging
+          width="auto"
+        >
+          <ColumnsDirective>
+            {employeesGrid.map((item, index) => (
+              <ColumnDirective key={index} {...item} />
+            ))}
+            <Inject services={[Search, Page, Toolbar]} />
+          </ColumnsDirective>
+        </GridComponent>
+      </div>
+    </>
+  );
 };
 
 export default Employees;
